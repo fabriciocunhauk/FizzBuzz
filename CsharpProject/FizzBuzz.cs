f0e46ce5ce2b67a495810e8c8f7e3954095117e4
fabricio cunha <fabriciocunhadeveloper@gmail.com>
2021-05-22 21:30:07 +0100

    Created object methods

---
+++ CsharpProject/FizzBuzz.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CsharpProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzzClass result = new FizzBuzzClass("Fizz", "Buzz", "FizzBuzz");
+
+            int num = 100;
+
+            for(int i = 1; i < num; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(result.Fizz);
+                } 
+                if(i % 5 == 0)
+                {
+                    Console.WriteLine(result.Buzz);
+                } 
+                if(i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine(result.FizzBuzz);
+                } else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ CsharpProject/FizzBuzzClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpProject
+{
+    class FizzBuzzClass
+    {
+            public string Fizz;
+            public string Buzz;
+            public string FizzBuzz;
+
+        public FizzBuzzClass(string aFizz, string aBuzz, string aFizzBuzz)
+        {
+            Fizz = aFizz;
+            Buzz = aBuzz;
+            FizzBuzz = aFizzBuzz;
+        }
+    }
+}
