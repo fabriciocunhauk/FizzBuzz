564c426941fedd234de4b68131be5bdbef2f6ff7
fabricio cunha <fabriciocunhadeveloper@gmail.com>
2021-05-22 02:42:25 +0100

    Add project files.

---
+++ CsharpProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CsharpProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "Fabricio";
+            int characterAge = 35;
+
+            Console.WriteLine("Hi my name is " + characterName);
+            Console.WriteLine("I am " + characterAge + " years old");
+        }
+    }
+}
