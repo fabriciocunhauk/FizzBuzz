d4fcc88ceb8dc1a5a404fd957f46c66a93090ce5
fabricio cunha <fabriciocunhadeveloper@gmail.com>
2021-05-22 03:28:44 +0100

    FizzBuzz challenge

--- CsharpProject/Program.cs
+++ CsharpProject/Program.cs
@@ -6,11 +6,24 @@ namespace CsharpProject
     {
         static void Main(string[] args)
         {
-            string characterName = "Fabricio";
-            int characterAge = 35;
+            int num = 100;
 
-            Console.WriteLine("Hi my name is " + characterName);
-            Console.WriteLine("I am " + characterAge + " years old");
+            for(int i = 0; i < num; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                } else if(i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                } else if(i % 15 == 0)
+                {
+                    Console.WriteLine("Fizz Buzz");
+                } else
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
